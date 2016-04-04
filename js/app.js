d96a08d091f5affe2a7e19e9e71b77110235a1b8
Hilary Masland <hilarymasland@gmail.com>
2016-04-04 09:26:06 -0400

    routerfunction

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function(){
+  angular
+  .module('breakEven'[
+    "ui.router",
+    "ngResource",
+    "days",
+    "exercises"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+})();
