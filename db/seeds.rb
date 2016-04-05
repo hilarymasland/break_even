49ba06ef282d3362ef526dc7bd0123000b6c647a
Hilary Masland <hilarymasland@gmail.com>
2016-04-05 13:37:00 -0400

    working to render JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+data = JSON.parse(File.read("db/exercises_data.json"))
+Exercise.destroy_all
+Exercise.create!(data)
