40b0bc5bfd3a87dadecc28cf8139aa04534ec801
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-05 18:46:46 -0400

    hardcoded seed file

--- db/migrate/20160405203730_create_drinks.rb
+++ db/migrate/20160405203730_create_drinks.rb
@@ -2,7 +2,7 @@ class CreateDrinks < ActiveRecord::Migration
   def change
     create_table :drinks do |t|
       t.string :drink_type
-      t.string :calorie_count
+      t.integer :calorie_count
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,28 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 data = JSON.parse(File.read("db/exercises_data.json"))
+# Exercise.destroy_all
+# Exercise.create!(data)
+
 Exercise.destroy_all
-Exercise.create!(data)
-Days.destroy_all
-Days.create!(data)
+Drink.destroy_all
+
+Exercise.create(exercise_name: "Stationary Bike", calories_burned: 8, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Elliptical", calories_burned: 11, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Running", calories_burned: 11, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Pull Ups", calories_burned: 9, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Push-ups", calories_burned: 9, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Sit-ups", calories_burned: 9, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Pilates", calories_burned: 3, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Yoga", calories_burned: 3, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Crunches", calories_burned: 3, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Aerobics", calories_burned: 8, quantity:'', reps:'', day:'')
+Exercise.create(exercise_name: "Dancing", calories_burned: 5, quantity:'', reps:'', day:'')
+
+
+Drink.create(drink_type:"glass of red wine", calorie_count:80)
+Drink.create(drink_type:"glass of white wine", calorie_count:80)
+Drink.create(drink_type:"bottle or can of beer", calorie_count:144)
+Drink.create(drink_type:"shot of scotch", calorie_count:65)
+Drink.create(drink_type:"shot of vodka", calorie_count:64)
+Drink.create(drink_type:"pint of beer", calorie_count:280)
