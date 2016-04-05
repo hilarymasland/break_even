088d1cec375ab2a0a09e4d0149b406aaf4560189
Hilary Masland <hilarymasland@gmail.com>
2016-04-05 19:10:50 -0400

    swapped infor in exercise and days.js files

--- app/assets/javascripts/day.module.js
+++ app/assets/javascripts/day.module.js
@@ -1,3 +1,4 @@
+
 // = require angular
 // = require angular-resource
 
@@ -6,7 +7,29 @@
 (function (){
   angular
   .module("days", [
-    "ngResource",
-    "exercises"
+    "ngResource"
+  ])
+  .controller("days_controller", [
+    "$resource",
+    DaysController
   ]);
+
+function DaysController($resource){
+  var vm = this
+  vm.data = data;
+  var Exercise = $resource("/exercises/:id.json", {}, {
+    update: {method: "PUT"}
+  });
+  vm.data = Exercise.query(function(response){
+  vm.data.forEach(function(exercise){
+    exercise.show = parseFloat(exercise.show);
+  });
+});
+//up above- code to make product cost show in inventory tracker - hopefully can be similar to displaying exercises HDM
+  vm.sort_data_by = function(name){
+    vm.sort_on = name;
+    vm.is_descending = !(vm.is_descending);
+  }
+}
+
 })();
