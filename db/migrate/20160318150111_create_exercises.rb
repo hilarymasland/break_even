365dce343081971213a7e2d938c22c342424b671
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-04-05 12:58:31 -0400

    edited excercise migration and repopulated schema

--- db/migrate/20160318150111_create_exercises.rb
+++ db/migrate/20160318150111_create_exercises.rb
@@ -2,6 +2,10 @@ class CreateExercises < ActiveRecord::Migration
   def change
     create_table :exercises do |t|
       t.string :name
+      t.string :image_url
+      t.integer :calories_burned
+      t.integer :quantity
+      t.integer :reps
       t.text :notes
       t.belongs_to :training, index: true, foreign_key: true
 
