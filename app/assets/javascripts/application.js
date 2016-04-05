6d3e8146175084388caa85b42d67e2b7130eb4e6
caseyagil <caseyagil@gmail.com>
2016-04-05 09:41:38 -0400

    Connecting AngularJs to Rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,46 @@
 //= require cocoon
 //= require jquery_ujs
 //= require turbolinks
+"use strict";
+
+(function(){
+  angular
+  .module("breakEven", [
+    "ui.Router",
+    "ngResource",
+    "exercises"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("Welcome", {
+      url: "",
+      templateUrl: "js/welcome.html"
+    })
+    .state("exercises", {
+      url: "/exercises",
+      templateUrl: "js/exercises/index.html",
+      controller: "ExercisesIndexController",
+      controllerAs: "ExercisesIndexViewModel"
+    })
+  } // closes Router Function
+}());
+.controller("break_even_controller", BreakEvenController);
+
+function BreakEvenController(){
+  var vm = this;
+  vm.data = data;
+  vm.total_value = function(){
+    var total = 0;
+    vm.data.forEach(function(product){
+      if(product.quantity){
+        total += (product.quantity * product.cost);
+      }
+    });
+    return total.toFixed(2);
+  }
+}
+})();
--- app/controllers/meals_controller.rb
+++
@@ -1,8 +0,0 @@
-class MealsController < ApplicationController
-   def create
-    @nutrition = Nutrition.find(params[:nutrition_id])
-    @meal = @nutrition.meals.create(params[:meal].permit(:feast, :name, :protein, :carbohydrates, :fat, :calories, :notes, :date))
-
-    redirect_to nutrition_path(@nutrition)
-  end
-end
