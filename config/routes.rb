f42b10902e570d46fc411cc4eda10a71ebad7601
Hilary Masland <hilarymasland@gmail.com>
2016-04-05 15:18:32 -0400

    delete sprockets - JSON working

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :trainings do
-    resources :exercises
-  end
 
-  resources :nutritions do
-    resources :meals
-  end
-  
+
+    resources :exercises
+
+
+
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
